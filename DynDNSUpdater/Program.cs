d5f75f973fbd048d96cac3147ba611df582f0d07
Chris House <chhouse@microsoft.com>
2024-03-16 13:49:32 -0500

    initial commit of worker, configuration loading successfully

--- DynDNSUpdater/Program.cs
+++ DynDNSUpdater/Program.cs
@@ -5,7 +5,16 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = Host.CreateApplicationBuilder(args);
+        builder.Configuration.AddEnvironmentVariables();
+        builder.Services.AddOptions<DynDnsUpdaterOptions>()
+            .Bind(builder.Configuration);
+
         builder.Services.AddHostedService<Worker>();
+
+        foreach (var c in builder.Configuration.AsEnumerable())
+        {
+            Console.WriteLine($"** {c.Key}: {c.Value}");
+        }
 
         var host = builder.Build();
         host.Run();
--- DynDNSUpdater/Worker.cs
+++ DynDNSUpdater/Worker.cs
@@ -1,23 +1,33 @@
+using Microsoft.Extensions.Options;
+
 namespace DynDNSUpdater;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IOptions<DynDnsUpdaterOptions> _options;
+    private readonly int delayIntervalInMilliseconds;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(IOptions<DynDnsUpdaterOptions> options, ILogger<Worker> logger)
     {
         _logger = logger;
+        _options = options;
+        delayIntervalInMilliseconds = options.Value.IntervalToCheckInMinutes * 60 * 1000;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation($"** Initializing worker, polling interval is {_options.Value.IntervalToCheckInMinutes} minutes/{delayIntervalInMilliseconds} ms");
+
+
         while (!stoppingToken.IsCancellationRequested)
         {
             if (_logger.IsEnabled(LogLevel.Information))
             {
+                Console.WriteLine($"** {_options.Value.HostNameToUpdate}");
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
-            await Task.Delay(1000, stoppingToken);
+            await Task.Delay(delayIntervalInMilliseconds, stoppingToken);
         }
     }
 }
