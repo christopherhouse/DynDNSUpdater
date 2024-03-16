2384d6806b32ba57703158cf387b1007f4b71f41
Christopher House <chhouse@microsoft.com>
2024-03-16 14:56:17 -0500

    Feature/http client impl (#1)
    * initial commit of worker, configuration loading successfully
    
    * adding orchestrator service
    
    * wiring in orchestration
    
    * updates working, need error handling

--- DynDNSUpdater/Program.cs
+++ DynDNSUpdater/Program.cs
@@ -1,3 +1,5 @@
+using DynDNSUpdater.Services;
+
 namespace DynDNSUpdater;
 
 public class Program
@@ -5,7 +7,14 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = Host.CreateApplicationBuilder(args);
+        builder.Configuration.AddEnvironmentVariables();
+        builder.Services.AddOptions<DynDnsUpdaterOptions>()
+            .Bind(builder.Configuration);
+
         builder.Services.AddHostedService<Worker>();
+        builder.Services.AddTransient<IDnsClient, DnsClient>();
+        builder.Services.AddTransient<IDnsUpdaterClient, DnsUpdaterClient>();
+        builder.Services.AddHttpClient<IDnsUpdateHttpClient, DnsUpdateHttpClient>();
 
         var host = builder.Build();
         host.Run();
--- DynDNSUpdater/Worker.cs
+++ DynDNSUpdater/Worker.cs
@@ -1,23 +1,40 @@
+using DynDNSUpdater.Services;
+using Microsoft.Extensions.Options;
+
 namespace DynDNSUpdater;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IOptions<DynDnsUpdaterOptions> _options;
+    private readonly IDnsUpdaterClient _dnsUpdaterClient;
+    private readonly int delayIntervalInMilliseconds;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(IOptions<DynDnsUpdaterOptions> options,
+        IDnsUpdaterClient dnsUpdaterClient,
+        ILogger<Worker> logger)
     {
         _logger = logger;
+        _options = options;
+        _dnsUpdaterClient = dnsUpdaterClient;
+        delayIntervalInMilliseconds = options.Value.IntervalToCheckInMinutes * 60 * 1000;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation($"** Initializing worker, polling interval is {_options.Value.IntervalToCheckInMinutes} minutes/{delayIntervalInMilliseconds} ms");
+
+
         while (!stoppingToken.IsCancellationRequested)
         {
             if (_logger.IsEnabled(LogLevel.Information))
             {
+                Console.WriteLine($"** {_options.Value.HostNameToUpdate}");
+
+                var result = await _dnsUpdaterClient.UpdateDnsAsync(_options.Value.HostNameToUpdate);
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
-            await Task.Delay(1000, stoppingToken);
+            await Task.Delay(delayIntervalInMilliseconds, stoppingToken);
         }
     }
 }
