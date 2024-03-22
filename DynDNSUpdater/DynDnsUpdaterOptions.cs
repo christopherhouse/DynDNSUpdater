6bf14c0bf6eef852b37eb4083e0746000172a9ce
Chris House <chhouse@microsoft.com>
2024-03-21 20:24:44 -0500

    updated options to include interval in ms, as an int

--- DynDNSUpdater/DynDnsUpdaterOptions.cs
+++ DynDNSUpdater/DynDnsUpdaterOptions.cs
@@ -3,9 +3,22 @@ namespace DynDNSUpdater;
 public class DynDnsUpdaterOptions
 {
     public string? IpCheckHostName { get; set; }
-    public int IntervalToCheckInMinutes { get; set; }
+    public string? IntervalToCheckInMinutes { get; set; }
     public string? DynDnsUpdateRequestUriFormat { get; set; }
     public string? HostNameToUpdate { get; set; }
     public string? DynDnsUsername { get; set; }
     public string? DynDnsPassword { get; set; }
+
+    public int IntervalToCheckInMinutesAsMilliseconds
+    {
+        get
+        {
+            if (int.TryParse(IntervalToCheckInMinutes, out var interval))
+            {
+                return interval * 60 * 1000;
+            }
+
+            throw new ArgumentException("IntervalToCheckInMinutes must be a number");
+        }
+    }
 }
